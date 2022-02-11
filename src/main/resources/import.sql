956f1d6f50782372edbbbda80424633f79d23699
JoseCedeño <jose.cedeno.est@tecazuay.edu.ec>
2022-02-11 10:21:20 -0500

    Inical

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into productos(descripcion,precio,cantidad,total) values ('Mesa', 0.50, 50,0);
+insert into productos(descripcion,precio,cantidad,total) values ('Casa', 0.50, 30,0);
+insert into productos(descripcion,precio,cantidad,total) values ('Camara', 0.50, 10,0);
+insert into productos(descripcion,precio,cantidad,total) values ('Telefono', 0.50, 60,0);
+insert into productos(descripcion,precio,cantidad,total) values ('Teclado', 0.50, 8,0);
